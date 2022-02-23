068484303347d1aba37adce670170019ddffceec
Beth Reddy <100309944+elizabeth-reddy@users.noreply.github.com>
2022-02-23 15:13:16 -0800

    Update MedianFilter.java

--- MedianFilter.java
+++ MedianFilter.java
@@ -22,7 +22,7 @@ public class MedianFilter {
 		// constructor with array of names of the noisy images
 			if (imageInputFilenames != null) {
 				int i;
-				images = new BufferedImage[imageInputFilenames.length];
+				images = new BufferedImage[imageInputFilenames.length]; /* read in files to image array */
 				for (i=0; i<imageInputFilenames.length; i++) {
 
 					File f = new File(imageInputFilenames[i]);
@@ -35,33 +35,29 @@ public class MedianFilter {
 		// opens and reads in an image file
 			BufferedImage img = null; 
 			try {
-				img = ImageIO.read(imageFile); // read file
+				img = ImageIO.read(imageFile);  /* read in file */
 			} catch (IOException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
+				e.printStackTrace(); /* print exception if file is not readable */
 			}
 			return img;
 		}
 		
 		public BufferedImage removeNoise() {
 		// gets the median value for all pixels and returns the filtered noiseless image
-			int i;
-			int j;
-			int x;
-			int k;
-			filteredImage = images[0];
+			int i, j, x, k
+			filteredImage = images[0]; /* instantiate with arbitrary image, first image will do */
 			ArrayList<Integer> pixels = new ArrayList<Integer>(images.length);
 			for (i=0; i<images.length; i++) {
-				pixels.add(0);
+				pixels.add(0); /* create array of one pixel to be compared for every image */
 			}
 			for (i=0; i<filteredImage.getHeight(); i++) {
 				for (j=0; j<filteredImage.getWidth(); j++) {
 					for (k = 0; k < pixels.size(); k++ ) {
-						pixels.set(k, (images[k].getRGB(j, i)));
+						pixels.set(k, (images[k].getRGB(j, i))); /* fill pixels array with each image at given pixel */
 					}
-					x = getMedianValue(pixels);
+					x = getMedianValue(pixels); /* find median value of each image at given pixel */
 					
-					filteredImage.setRGB(j, i, x);
+					filteredImage.setRGB(j, i, x); /* set filtered image to the median pixel */
 				}
 			}
 			return filteredImage;
@@ -70,22 +66,20 @@ public class MedianFilter {
 		
 		public int getMedianValue(ArrayList<Integer> pixels) {
 		// returns the median value of the pixel(x,y) for all images
-			int x;
-			Collections.sort(pixels);
-			x = pixels.get(pixels.size()/2);
-			return x;
+			Collections.sort(pixels); /* sorts the array */
+			return pixels.get(pixels.size()/2); /* selects the median of the sorted array */
 		}
 		
 		public int writeImage(String outputFilename) {
 		// writes filteredImage to the outputFilename jpg file. Returns 0 if successful, or -1 if an exception was thrown.
 			File f2 = new File(outputFilename);
 			try {
-				ImageIO.write(filteredImage, "jpg", f2);
+				ImageIO.write(filteredImage, "jpg", f2); /* writes output to a file */
 				return 0;
 			} catch (IOException e) {
-				System.out.print("Error writing file");
+				System.out.print("Error writing file"); /* catches an IOException when writing file */
 				return -1;
-			} // write to file
+			}
 		}
 		
 		public int getHeight() {
